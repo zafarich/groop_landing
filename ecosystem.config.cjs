489104073b8070bb55be1dbac8344208137523ef
zafar <webzafar6699@gmail.com>
2025-12-18 00:08:35 +0500

    asd

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -6,7 +6,7 @@ module.exports = {
       exec_mode: "fork",
       instances: 1,
       script: "./.output/server/index.mjs",
-      max_memory_restart: "800M",
+      max_memory_restart: "512M",
       env: {
         NODE_ENV: "production",
         PORT: 4004,
