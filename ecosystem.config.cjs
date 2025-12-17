ba7e653cc970835ec1babf0905f43ec1602ea7e0
zafar <webzafar6699@gmail.com>
2025-12-17 20:25:25 +0500

    ads

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,9 +3,10 @@ module.exports = {
     {
       name: "groop-landing",
       port: "4004",
-      exec_mode: "cluster",
-      instances: "max",
+      exec_mode: "fork",
+      instances: 1,
       script: "./.output/server/index.mjs",
+      max_memory_restart: "800M",
       env: {
         NODE_ENV: "production",
         PORT: 4004,
